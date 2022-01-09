de4ef3ab84e0c440091d87db08c267f16ace6b4f
Jinsol <jinsolkim91@gmail.com>
2022-01-09 16:05:38 +0900

    first commit

---
+++ models/campground.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Review = require('./review');
+const { campgroundSchema } = require('../schemas');
+const Schema = mongoose.Schema;
+
+const CampGroundSchema = new Schema({
+    title: String, 
+    image: String,
+    price: Number, 
+    description: String, 
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampGroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampGroundSchema);
+  
